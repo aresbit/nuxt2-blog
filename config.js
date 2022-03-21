4d6dd2d313e1cdd9ac21e5f4255d38d43219cbd2
aresbit <33110495+aresbit@users.noreply.github.com>
2022-03-22 07:47:30 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 export default {
-  title: 'yun yu yuan',
-  githubName: 'yunyuyuan',
+  title: 'aresyang',
+  githubName: 'aresbit',
   githubRepo: 'nuxt2-blog',
   githubBranch: 'master',
   domain: 'https://blog.halberd.cn',
-  SEO_description: '云与原的博客，书写值得书写的东西',
-  SEO_keywords: 'yunyuyuan,yunyuyuan\'s blog,云与原,云与原的博客',
+  SEO_description: 'aresyang的博客，记录美好的每一天呐',
+  SEO_keywords: 'ares,aresyang\'s blog,aresyang的博客',
   CloudflareAnalyze: "92cdf13c9c964ad1ab71c638c6fde0fc"
 }
